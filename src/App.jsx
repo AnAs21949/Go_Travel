8da87f60a6954c4c97e5bde30712c601ada95e3b
AnAs21949 <ayousbest2001@gmail.com>
2023-05-24 11:29:46 +0100

    dd

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 
-import Nav from './Components/Nav/Nav' 
+import Nav from './Components/Nav/Nav.jsx' 
 import Home from './Components/Home/Home' 
 import Popular from './Components/popular/Popular' 
 import Offers from './Components/Offers/Offers' 
